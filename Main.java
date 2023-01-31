9d47515b98946412a8fd359394bcd690edf18ea1
MaksD88 <123211224+MaksD88@users.noreply.github.com>
2023-01-31 12:14:49 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        float ticketPrise = 2_000;
-        int milePrise = 20;
-        int miles = (int)ticketPrise / milePrise;
-        System.out.println("Вам будет начислено " + miles + " миль за купленный билет.");
-    }
-}
