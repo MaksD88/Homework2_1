068fb81a1f09dfb14739830564761b853d16ba3d
MaksD88 <123211224+MaksD88@users.noreply.github.com>
2023-01-31 12:15:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String [] args) {
-        float ticketPrise = 2_000;
+        float ticketPrise = 2_020;
         int milePrise = 20;
         int miles = (int)ticketPrise / milePrise;
         System.out.println("Вам будет начислено " + miles + " миль за купленный билет.");
